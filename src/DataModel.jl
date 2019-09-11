acbad8dd2dae22b96bb26cef72e7656be15f6090
Qi Zhang <singularitti@outlook.com>
2019-09-10 21:18:41 -0400

    Add a new field to `Cell` type: `magmoms`
    Using Parameters.jl, the default value is `nothing`

--- src/DataModel.jl
+++ src/DataModel.jl
@@ -11,12 +11,15 @@ julia>
 """
 module DataModel
 
+using Parameters
+
 export Cell
 
-struct Cell{L<:AbstractMatrix,P<:AbstractMatrix,N<:AbstractVector}
+struct Cell{L<:AbstractMatrix,P<:AbstractMatrix,N<:AbstractVector,M<:Union{AbstractVector,Nothing}}
     lattice::L
     positions::P
     numbers::N
+    magmoms::M = nothing
 end
 
 end
