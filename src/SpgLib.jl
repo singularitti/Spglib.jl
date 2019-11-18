b476b57bf919b8e35d8a08225aa25193c0009b98
Qi Zhang <singularitti@outlook.com>
2019-11-18 05:31:13 -0500

    Deprecate DataModel.jl
    move to SpgLib.jl

--- src/SpgLib.jl
+++ src/SpgLib.jl
@@ -1,6 +1,17 @@
 module SpgLib
 
-# include("DataModel.jl")
+using Parameters: @with_kw
+
+export Cell
+
+@with_kw struct Cell{L<:AbstractMatrix,P<:AbstractMatrix,N<:AbstractVector,M<:Union{AbstractVector,Nothing}}
+    lattice::L
+    positions::P
+    numbers::N
+    magmoms::M = nothing
+end
+Cell(lattice, positions, numbers) = Cell(lattice, positions, numbers, nothing)
+
 include("Wrapper/Wrapper.jl")
 # include("FFI.jl")
 
