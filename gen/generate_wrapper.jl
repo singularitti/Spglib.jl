2fe822f5c17bf55a1b5a264a32b96a932c2ff345
Qi Zhang <singularitti@outlook.com>
2019-11-18 04:39:09 -0500

    :wrench: Rewrite gen/generate_wrapper.jl

--- gen/generate_wrapper.jl
+++ gen/generate_wrapper.jl
@@ -4,10 +4,10 @@ HEADER_BASE = "./deps/usr/include" # using system-wide installation of SCIP
 all_headers = readdir(joinpath(HEADER_BASE, "spglib"))
 context = Clang.init(
     headers = [joinpath(HEADER_BASE, "spglib", h) for h in all_headers],
-    common_file = "commons.jl",
-    output_dir = "./src/wrapper/",
-    output_file = "capi.jl",
+    common_file = "spglib_common.jl",
+    output_dir = "./src/Wrapper/",
+    output_file = "spglib_api.jl",
     clang_args = ["-I", HEADER_BASE],
-    header_wrapped = (header, cursorname)->header == cursorname,
+    header_wrapped = (header, cursorname) -> header == cursorname,
 )
 Clang.run(context)
