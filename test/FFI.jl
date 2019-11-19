8b388944e72c231794bbfb4f993fb4b354b34afa
Qi Zhang <singularitti@outlook.com>
2019-11-19 05:37:21 -0500

    :white_check_mark: Add "Test distorted rutile structure"

--- test/FFI.jl
+++ test/FFI.jl
@@ -21,3 +21,21 @@ using SpgLib.FFI
     rutile = Cell(lattice, positions, numbers) |> SpgLib.FFI.get_ccell
     # get_symmetry(rutile; symprec = 1e-5)
 end # testset
+
+@testset "Test distorted rutile structure" begin
+    lattice = [
+        3.97 0.00 0.00
+        0.00 4.03 0.00
+        0.00 0.00 3.00
+    ]
+    positions = [
+        0 0 0
+        0.5001 0.5 0.5
+        0.3 0.3 0.0
+        0.7 0.7 0.002
+        0.2 0.8 0.5
+        0.8 0.2 0.5
+    ]
+    numbers = [14, 14, 8, 8, 8, 8]
+    distorted_rutile = Cell(lattice, positions, numbers)
+end # testset
