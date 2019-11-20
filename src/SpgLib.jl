6530c6a315a5bd15548d425eff76b91eec4053ea
Qi Zhang <singularitti@outlook.com>
2019-11-20 04:52:21 -0500

    :sparkles: :alembic: Add type `SpaceGroup` corresponds to `SpglibSpacegroupType`

--- src/SpgLib.jl
+++ src/SpgLib.jl
@@ -2,7 +2,7 @@ module SpgLib
 
 using Parameters: @with_kw
 
-export Cell
+export Cell, SpaceGroup
 
 @with_kw struct Cell{L<:AbstractVecOrMat,P<:AbstractVecOrMat,N<:AbstractVector,M<:Union{AbstractVector,Nothing}}
     lattice::L
@@ -11,6 +11,20 @@ export Cell
     magmoms::M = nothing
 end
 Cell(lattice, positions, numbers) = Cell(lattice, positions, numbers, nothing)
+
+struct SpaceGroup
+    number::Int
+    international_short::String
+    international_full::String
+    international::String
+    schoenflies::String
+    hall_symbol::String
+    choice::String
+    pointgroup_international::String
+    pointgroup_schoenflies::String
+    arithmetic_crystal_class_number::Int
+    arithmetic_crystal_class_symbol::String
+end
 
 include("Wrapper/Wrapper.jl")
 include("FFI.jl")
