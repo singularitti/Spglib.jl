621193c367620393efc1441d827671d2772d5545
Qi Zhang <singularitti@outlook.com>
2019-11-20 06:00:48 -0500

    :alembic: Add type `Dataset`

--- src/SpgLib.jl
+++ src/SpgLib.jl
@@ -2,7 +2,7 @@ module SpgLib
 
 using Parameters: @with_kw
 
-export Cell, SpaceGroup
+export Cell, Dataset, SpaceGroup
 
 @with_kw struct Cell{L<:AbstractVecOrMat,P<:AbstractVecOrMat,N<:AbstractVector,M<:Union{AbstractVector,Nothing}}
     lattice::L
@@ -11,6 +11,27 @@ export Cell, SpaceGroup
     magmoms::M = nothing
 end
 Cell(lattice, positions, numbers) = Cell(lattice, positions, numbers, nothing)
+
+struct Dataset
+    spacegroup_number::Int
+    hall_number::Int
+    international_symbol::String
+    hall_symbol::String
+    choice::String
+    transformation_matrix::Matrix{Float64}
+    origin_shift::Vector{Float64}
+    n_operations::Int
+    rotations::Ptr{Cvoid}
+    translations::Ptr{Cvoid}
+    n_atoms::Int
+    wyckoffs::Ptr{Int}
+    equivalent_atoms::Ptr{Int}
+    n_std_atoms::Int
+    std_lattice::Matrix{Float64}
+    std_types::Ptr{Int}
+    std_positions::Ptr{Cvoid}
+    pointgroup_symbol::String
+end
 
 struct SpaceGroup
     number::Int
