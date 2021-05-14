a24798d4bceab794600290b51c303019614395d0
Documenter.jl <documenter@juliadocs.github.io>
2021-05-14 23:31:14 +0000

    build based on c538546

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Spglib.jl","page":"Home","title":"Spglib.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Spglib]","category":"page"}]
+[{"location":"#Spglib.jl","page":"Home","title":"Spglib.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Spglib]","category":"page"},{"location":"#Spglib.get_multiplicity","page":"Home","title":"Spglib.get_multiplicity","text":"get_multiplicity(cell::Cell, symprec = 1e-8)\n\nReturn the exact number of symmetry operations. An error is thrown when it fails.\n\n\n\n\n\n","category":"function"}]
 }
