6b93690fbecd358f9dd50ac442223f5f73730ff4
Documenter.jl <documenter@juliadocs.github.io>
2021-05-14 21:32:26 +0000

    build based on 7031d54

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Spglib.jl","page":"Home","title":"Spglib.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Spglib]","category":"page"}]
+}
