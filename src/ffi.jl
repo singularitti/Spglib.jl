65cfe924a0531b5f971ec291e23cba6d1a02f580
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-16 18:27:30 -0400

    Add docs for `standardize_cell`, `find_primitive` & `refine_cell`

--- src/ffi.jl
+++ src/ffi.jl
@@ -256,6 +256,15 @@ function get_schoenflies(cell::Cell, symprec = 1e-8)
 end
 
 # See https://github.com/spglib/spglib/blob/444e061/python/spglib/spglib.py#L415-L463 and https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/src/cell-reduce-api.jl#L3-L35
+"""
+    standardize_cell(cell::Cell; to_primitive = false, no_idealize = false, symprec = 1e-5)
+
+Return standardized cell.
+
+The standardized unit cell is generated from an input unit cell structure and
+its symmetry found by the symmetry search. The choice of the setting for each
+space group type is as explained for [`get_dataset`](@ref).
+"""
 function standardize_cell(
     cell::Cell;
     to_primitive = false,
@@ -287,9 +296,29 @@ function standardize_cell(
     return Cell(lattice, _positions[:, 1:num_atom_std], _types[1:num_atom_std])
 end
 
+"""
+    find_primitive(cell::Cell, symprec = 1e-5)
+
+Find the primitive cell of an input unit cell.
+
+This function is now a shortcut of `standardize_cell` with `to_primitive = true`
+and `no_idealize = false`.
+"""
 find_primitive(cell::Cell, symprec = 1e-5) =
     standardize_cell(cell; to_primitive = true, no_idealize = false, symprec = symprec)
 
+
+"""
+    refine_cell(cell::Cell, symprec = 1e-5)
+
+Return refined cell.
+
+The standardized crystal structure is obtained from a non-standard crystal
+structure which may be slightly distorted within a symmetry recognition
+tolerance, or whose primitive vectors are differently chosen, etc.
+This function is now a shortcut of `standardize_cell` with `to_primitive = false`
+and `no_idealize = false`.
+"""
 refine_cell(cell::Cell, symprec = 1e-5) =
     standardize_cell(cell; to_primitive = false, no_idealize = false, symprec = symprec)
 
