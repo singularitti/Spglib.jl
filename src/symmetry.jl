7d7be4a77155241a38381a7a0330338d3f306017
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-27 01:53:40 -0400

    Fix docs of `get_spacegroup_number`

--- src/symmetry.jl
+++ src/symmetry.jl
@@ -231,7 +231,7 @@ end
 """
     get_spacegroup_number(cell::Cell, symprec=1e-5)
 
-Get the spacegroup number from the Spglib dataset.
+Get the spacegroup number of a `cell`.
 """
 function get_spacegroup_number(cell::Cell, symprec = 1e-5)
     dataset = get_dataset(cell, symprec = symprec)
