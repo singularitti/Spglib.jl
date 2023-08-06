b7bf4fdab083d53b944344fea7a4cbb4c2b11510
singularitti <singularitti@outlook.com>
2023-08-06 08:40:44 -0400

    Recover `SpglibError` type removed from #115 & add `check_error`

--- src/error.jl
+++ src/error.jl
@@ -1,4 +1,4 @@
-export get_error_code, get_error_message
+export get_error_code, get_error_message, check_error
 
 # See https://www.mcobject.com/docs/Content/Programming/C/Return_Codes.htm or
 # https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
@@ -15,8 +15,24 @@ export get_error_code, get_error_message
     NONE
 end
 
+struct SpglibError <: Exception
+    msg::String
+end
+
 get_error_code() = ccall((:spg_get_error_code, libsymspg), SpglibReturnCode, ())
 
 get_error_message(spglib_error::SpglibReturnCode) = unsafe_string(
     ccall((:spg_get_error_message, libsymspg), Cstring, (SpglibReturnCode,), spglib_error),
 )
+
+function check_error()
+    code = get_error_code()
+    if code == SUCCESS
+        return nothing
+    else
+        return throw(SpglibError(get_error_message(code)))
+    end
+end
+
+# See https://github.com/JuliaLang/julia/blob/3903fa5/base/missing.jl#L18-L19
+Base.showerror(io::IO, ex::SpglibError) = print(io, "SpglibError: ", ex.msg, '!')
