429dcb9d58917ec97b8801e310e3beb76ca51bf0
singularitti <singularitti@outlook.com>
2023-08-06 08:31:31 -0400

    Rename `SpglibError` to `SpglibReturnCode` to better match its usage
    Remove `SPGLIB_` & `SPGERR_` in enum type

--- src/error.jl
+++ src/error.jl
@@ -1,20 +1,22 @@
 export get_error_code, get_error_message
 
-@enum SpglibError begin
-    SPGLIB_SUCCESS = 0
-    SPGERR_SPACEGROUP_SEARCH_FAILED
-    SPGERR_CELL_STANDARDIZATION_FAILED
-    SPGERR_SYMMETRY_OPERATION_SEARCH_FAILED
-    SPGERR_ATOMS_TOO_CLOSE
-    SPGERR_POINTGROUP_NOT_FOUND
-    SPGERR_NIGGLI_FAILED
-    SPGERR_DELAUNAY_FAILED
-    SPGERR_ARRAY_SIZE_SHORTAGE
-    SPGERR_NONE
+# See https://www.mcobject.com/docs/Content/Programming/C/Return_Codes.htm or
+# https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+@enum SpglibReturnCode begin
+    SUCCESS  # 0
+    SPACEGROUP_SEARCH_FAILED
+    CELL_STANDARDIZATION_FAILED
+    SYMMETRY_OPERATION_SEARCH_FAILED
+    ATOMS_TOO_CLOSE
+    POINTGROUP_NOT_FOUND
+    NIGGLI_FAILED
+    DELAUNAY_FAILED
+    ARRAY_SIZE_SHORTAGE
+    NONE
 end
 
-get_error_code() = ccall((:spg_get_error_code, libsymspg), SpglibError, ())
+get_error_code() = ccall((:spg_get_error_code, libsymspg), SpglibReturnCode, ())
 
-get_error_message(spglib_error::SpglibError) = unsafe_string(
-    ccall((:spg_get_error_message, libsymspg), Cstring, (SpglibError,), spglib_error)
+get_error_message(spglib_error::SpglibReturnCode) = unsafe_string(
+    ccall((:spg_get_error_message, libsymspg), Cstring, (SpglibReturnCode,), spglib_error),
 )
