49a2a2efe0f3a0174961d495b2abb0bab0f2f69e
singularitti <singularitti@outlook.com>
2023-08-06 03:04:55 -0400

    Add `get_error_message`

--- src/error.jl
+++ src/error.jl
@@ -1,3 +1,5 @@
+export get_error_code, get_error_message
+
 @enum SpglibError begin
     SPGLIB_SUCCESS = 0
     SPGERR_SPACEGROUP_SEARCH_FAILED
@@ -12,3 +14,7 @@
 end
 
 get_error_code() = ccall((:spg_get_error_code, libsymspg), SpglibError, ())
+
+get_error_message(spglib_error::SpglibError) = unsafe_string(
+    ccall((:spg_get_error_message, libsymspg), Cstring, (SpglibError,), spglib_error)
+)
