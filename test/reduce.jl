38848beab8cea43406e9c57a104c19a3c83bc0f0
singularitti <singularitti@outlook.com>
2023-08-06 21:51:43 -0400

    Use the `Lattice` constructor for `lattice` initialization in `[test/reduce.jl]`

--- test/reduce.jl
+++ test/reduce.jl
@@ -1,10 +1,10 @@
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L83-L85
 @testset "Niggli reduce" begin
-    lattice = [
+    lattice = Lattice([
         4.0 0.0 0.0
         20.0 2.0 0.0
         0.0 0.0 12.0
-    ]
+    ])
     @test niggli_reduce(lattice, 1e-3) ≈ [
         0.0 4.0 0.0
         -2.0 0.0 0.0
@@ -19,11 +19,11 @@
 end
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L87-89
 @testset "Delaunay reduce" begin
-    lattice = [
+    lattice = Lattice([
         4.0 0.0 0.0
         20.0 2.0 0.0
         0.0 0.0 12.0
-    ]
+    ])
     @test delaunay_reduce(lattice, 1e-3) ≈ [
         0.0 -4.0 0.0
         2.0 0.0 0.0
