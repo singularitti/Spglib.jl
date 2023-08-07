b5ac6b1527ad90d09442d183b89d7a1eb8980992
singularitti <singularitti@outlook.com>
2023-08-07 19:48:53 -0400

    Add types `SpglibMagneticDataset` & `MagneticDataset` in src/magnetic.jl

---
+++ src/magnetic.jl
@@ -0,0 +1,43 @@
+struct SpglibMagneticDataset
+    uni_number::Cint
+    msg_type::Cint
+    hall_number::Cint
+    tensor_rank::Cint
+    n_operations::Cint
+    rotations::Ptr{Cint}
+    translations::Ptr{Cdouble}
+    time_reversals::Ptr{Cint}
+    n_atoms::Cint
+    equivalent_atoms::Ptr{Cint}
+    transformation_matrix::NTuple{9,Cdouble}
+    origin_shift::NTuple{3,Cdouble}
+    n_std_atoms::Cint
+    std_lattice::NTuple{9,Cdouble}
+    std_types::Ptr{Cint}
+    std_positions::Ptr{Cdouble}
+    std_tensors::Ptr{Cdouble}
+    std_rotation_matrix::NTuple{9,Cdouble}
+    primitive_lattice::NTuple{9,Cdouble}
+end
+
+struct MagneticDataset
+    uni_number::Int32
+    msg_type::Int32
+    hall_number::Int32
+    tensor_rank::Int32
+    n_operations::Int32
+    rotations::Vector{SMatrix{3,3,Int32,9}}
+    translations::Vector{SVector{3,Float64}}
+    time_reversals::Vector{Int32}
+    n_atoms::Int32
+    equivalent_atoms::Vector{Int32}
+    transformation_matrix::SMatrix{3,3,Float64,9}
+    origin_shift::SVector{3,Float64}
+    n_std_atoms::Int32
+    std_lattice::Lattice{Float64}
+    std_types::Vector{Int32}
+    std_positions::Vector{SVector{3,Float64}}
+    std_tensors::Vector{Float64}
+    std_rotation_matrix::SMatrix{3,3,Float64,9}
+    primitive_lattice::Lattice{Float64}
+end
