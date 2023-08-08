e4a88d9104b8b46fa42dd26f536d0bdf2d803685
singularitti <singularitti@outlook.com>
2023-08-07 23:41:04 -0400

    Add `Base.convert(::Type{MagneticDataset}, dataset::SpglibMagneticDataset)`

--- src/magnetic.jl
+++ src/magnetic.jl
@@ -41,3 +41,46 @@ struct MagneticDataset
     std_rotation_matrix::SMatrix{3,3,Float64,9}
     primitive_lattice::Lattice{Float64}
 end
+
+function Base.convert(::Type{MagneticDataset}, dataset::SpglibMagneticDataset)
+    rotations = [
+        _convert(SMatrix{3,3,Int32}, unsafe_load(dataset.rotations, i)) for
+        i in Base.OneTo(dataset.n_operations)
+    ]
+    translations = [
+        SVector{3}(unsafe_load(dataset.translations, i)) for
+        i in Base.OneTo(dataset.n_operations)
+    ]
+    time_reversals = unsafe_wrap(
+        Vector{Int32}, dataset.time_reversals, dataset.n_operations
+    )
+    equivalent_atoms = unsafe_wrap(Vector{Int32}, dataset.equivalent_atoms, dataset.n_atoms)
+    std_lattice = Lattice(transpose(_convert(SMatrix{3,3,Float64}, dataset.std_lattice)))
+    std_types = unsafe_wrap(Vector{Int32}, dataset.std_types, dataset.n_std_atoms)
+    std_positions = [
+        SVector{3}(unsafe_load(dataset.std_positions, i)) for
+        i in Base.OneTo(dataset.n_std_atoms)
+    ]
+    std_tensors = unsafe_wrap(Vector{Float64}, dataset.std_tensors, dataset.n_std_atoms)
+    return MagneticDataset(
+        dataset.uni_number,
+        dataset.msg_type,
+        dataset.hall_number,
+        dataset.tensor_rank,
+        dataset.n_operations,
+        rotations,
+        translations,
+        time_reversals,
+        dataset.n_atoms,
+        equivalent_atoms,
+        dataset.transformation_matrix,
+        dataset.origin_shift,
+        dataset.n_std_atoms,
+        std_lattice,
+        std_types,
+        std_positions,
+        std_tensors,
+        dataset.std_rotation_matrix,
+        dataset.primitive_lattice,
+    )
+end
