4bfa160ddb3873f57f72b906212a51242a6f4072
singularitti <singularitti@outlook.com>
2023-08-09 00:21:56 -0400

    Fix `standardize_cell`

--- src/standardize.jl
+++ src/standardize.jl
@@ -8,34 +8,29 @@ The standardized unit cell is generated from an input unit cell structure and
 its symmetry found by the symmetry search. The choice of the setting for each
 space group type is as explained for [`get_dataset`](@ref).
 """
-function standardize_cell(cell::Cell; to_primitive=false, no_idealize=false, symprec=1e-5)
-    lattice, positions, types = _expand_cell(cell)
-    to_primitive = Base.cconvert(Cint, to_primitive)
-    no_idealize = Base.cconvert(Cint, no_idealize)
-    num_atom = Base.cconvert(Cint, length(types))
+function standardize_cell(
+    cell::AbstractCell; to_primitive=false, no_idealize=false, symprec=1e-5
+)
+    lattice, _positions, _atoms = _expand_cell(cell)
+    n = natoms(cell)
     allocations = 4
-    _positions = Matrix{Cdouble}(undef, 3, num_atom * allocations)
-    _types = Vector{Cint}(undef, num_atom * allocations)
-    _positions[:, 1:num_atom] = positions
-    _types[1:num_atom] = types
-    num_atom_std = ccall(
-        (:spg_standardize_cell, libsymspg),
-        Cint,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cint}, Cint, Cint, Cint, Cdouble),
-        lattice,
-        _positions,
-        _types,
-        num_atom,
-        to_primitive,
-        no_idealize,
-        symprec,
-    )  # Note: not `num_atom`!
+    positions = Matrix{Cdouble}(undef, 3, n * allocations)
+    atoms = Vector{Cint}(undef, n * allocations)
+    positions[:, 1:n] .= _positions
+    atoms[1:n] .= _atoms
+    n_std = @ccall libsymspg.spg_standardize_cell(
+        lattice::Ptr{Cdouble},
+        positions::Ptr{Cdouble},
+        atoms::Ptr{Cint},
+        n::Cint,
+        to_primitive::Cint,
+        no_idealize::Cint,
+        symprec::Cdouble,
+    )::Cint  # Note: not `n`!
     check_error()
     # We have to `transpose` back because of `_expand_cell`!
     return Cell(
-        Lattice(transpose(lattice)),
-        collect(eachcol(_positions))[1:num_atom_std],
-        _types[1:num_atom_std],
+        Lattice(transpose(lattice)), collect(eachcol(positions[:, 1:n_std])), atoms[1:n_std]
     )
 end
 
@@ -47,7 +42,7 @@ Find the primitive cell of an input unit cell.
 This function is now a shortcut of `standardize_cell` with `to_primitive = true`
 and `no_idealize = false`.
 """
-find_primitive(cell::Cell, symprec=1e-5) =
+find_primitive(cell::AbstractCell, symprec=1e-5) =
     standardize_cell(cell; to_primitive=true, no_idealize=false, symprec=symprec)
 
 """
@@ -61,5 +56,5 @@ tolerance, or whose primitive vectors are differently chosen, etc.
 This function is now a shortcut of `standardize_cell` with `to_primitive = false`
 and `no_idealize = false`.
 """
-refine_cell(cell::Cell, symprec=1e-5) =
+refine_cell(cell::AbstractCell, symprec=1e-5) =
     standardize_cell(cell; to_primitive=false, no_idealize=false, symprec=symprec)
