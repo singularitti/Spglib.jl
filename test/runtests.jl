be1fdf1103b48339eb06cc398bd8855d3724f841
singularitti <singularitti@outlook.com>
2023-08-09 01:41:08 -0400

    Move test "Construct a `Cell`" to test/cell.jl
    Remove duplicated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 @testset "Spglib.jl" begin
     # Write your own tests here.
+    include("cell.jl")
     include("symmetry.jl")
     include("standardize.jl")
     include("reciprocal.jl")
