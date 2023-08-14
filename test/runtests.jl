8b6768cc06c1dbd5db626e54b33ae45acb7ce712
singularitti <singularitti@outlook.com>
2023-08-14 19:26:41 -0400

    Comment code for `magnetic` code & tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "Spglib.jl" begin
     # Write your own tests here.
     include("misc.jl")
-    include("magnetic.jl")
+    # include("magnetic.jl")
     include("standardize.jl")
     include("reciprocal.jl")
     include("reduce.jl")
