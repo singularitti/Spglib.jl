9b481b8da9650c7b00401332ee1dfd2a5de0c173
singularitti <singularitti@outlook.com>
2023-08-14 17:50:35 -0400

    Add "Test Niggli reduction from Python example"

--- test/reduce.jl
+++ test/reduce.jl
@@ -1,3 +1,16 @@
+# From https://github.com/spglib/spglib/blob/d8c39f6/example/python_api/example_full.py#L119-L122
+@testset "Test Niggli reduction from Python example" begin
+    𝐚 = [3, 0, 0]
+    𝐛 = [-3.66666667, 3.68178701, 0]
+    𝐜 = [-0.66666667, -1.3429469, 1.32364995]
+    lattice = Lattice(𝐚, 𝐛, 𝐜)
+    @test niggli_reduce(lattice, 1e-5) ≈ Lattice([
+        [-0.66666667, -1.3429469, 1.32364995],
+        [2.33333333, -1.3429469, 1.32364995],
+        [0.99999999, 0.99589321, 2.6472999],
+    ])
+end
+
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L83-L85
 @testset "Niggli reduce" begin
     lattice = Lattice([
