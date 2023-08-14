a93a590a39cbd6be7bcdfaf23791139ca7704d07
singularitti <singularitti@outlook.com>
2023-08-14 17:51:07 -0400

    Fix "Test Niggli reduction"

--- test/reduce.jl
+++ test/reduce.jl
@@ -12,17 +12,13 @@
 end
 
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L83-L85
-@testset "Niggli reduce" begin
+@testset "Test Niggli reduction" begin
     lattice = Lattice([
-        4.0 0.0 0.0
-        20.0 2.0 0.0
-        0.0 0.0 12.0
-    ])
-    @test niggli_reduce(lattice, 1e-3) ≈ [
-        0.0 4.0 0.0
-        -2.0 0.0 0.0
-        0.0 0.0 12.0
-    ]
+        4 0 0
+        20 2 0
+        0 0 12
+    ])  # Note in `LibSymspg.jl`, the lattice is transposed
+    @test niggli_reduce(lattice, 1e-3) == Lattice([[0, -2, 0], [4, 0, 0], [0, 0, 12]])  # Compared also with Python results
     cell = Cell(lattice, [[0.0, 0.0, 0.0], [0.05, 0.05, 0.05]], [1, 1])
     rcell = niggli_reduce(cell)
     c1 = Ref(cell.lattice) .* cell.positions
@@ -30,6 +26,7 @@ end
     # Cartesian coordinates should remain the same
     @test c1 == c2
 end
+
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L87-89
 @testset "Delaunay reduce" begin
     lattice = Lattice([
