48b3e3b99243295462f58a23986e3c4f1155ae72
singularitti <singularitti@outlook.com>
2023-08-14 19:47:00 -0400

    Add docs in src/error.jl

--- src/error.jl
+++ src/error.jl
@@ -2,6 +2,7 @@ export get_error_code, get_error_message
 
 # See https://www.mcobject.com/docs/Content/Programming/C/Return_Codes.htm or
 # https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+"Represent various return codes from the Spglib library."
 @enum SpglibReturnCode begin
     SUCCESS  # 0
     SPACEGROUP_SEARCH_FAILED
@@ -19,11 +20,26 @@ struct SpglibError <: Exception
     msg::String
 end
 
-get_error_code() = unsafe_string(@ccall libsymspg.spg_get_error_code::SpglibReturnCode())
+"""
+    get_error_code()
 
+Return an instance of the enumerated type `SpglibReturnCode`.
+"""
+get_error_code() = unsafe_string(@ccall libsymspg.spg_get_error_code()::SpglibReturnCode)
+
+"""
+    get_error_message(code::SpglibReturnCode)
+
+Get the corresponding error message for a given error code.
+"""
 get_error_message(code::SpglibReturnCode) =
     unsafe_string(@ccall libsymspg.spg_get_error_message(code::SpglibReturnCode)::Cstring)
 
+"""
+    check_error()
+
+Check if an error has occurred and throw an exception if so.
+"""
 function check_error()
     code = get_error_code()
     if code == SUCCESS
