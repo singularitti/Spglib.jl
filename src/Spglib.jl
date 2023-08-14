9ae5a802af9f4ebb5de7abe1c6686afd1313e4ec
singularitti <singularitti@outlook.com>
2023-08-14 01:19:19 -0400

    Revamped functionality with the transition from `ccall` to `@ccall`

--- src/Spglib.jl
+++ src/Spglib.jl
@@ -30,9 +30,9 @@ Obtain the version number of `spglib`.
 This is the mergence of `spg_get_major_version`, `spg_get_minor_version`, and `spg_get_micro_version` in its C-API.
 """
 function get_version()
-    major = ccall((:spg_get_major_version, libsymspg), Cint, ())
-    minor = ccall((:spg_get_minor_version, libsymspg), Cint, ())
-    micro = ccall((:spg_get_micro_version, libsymspg), Cint, ())
+    major = @ccall libsymspg.spg_get_major_version()::Cint
+    minor = @ccall libsymspg.spg_get_minor_version()::Cint
+    micro = @ccall libsymspg.spg_get_micro_version()::Cint
     return VersionNumber(major, minor, micro)
 end
 
