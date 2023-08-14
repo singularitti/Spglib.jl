89d47c6d96085720647eafb7400cb716a7801580
singularitti <singularitti@outlook.com>
2023-08-14 18:29:07 -0400

    Update "Test Niggli reduction from Python example" to "Test examples from Python"

--- test/reduce.jl
+++ test/reduce.jl
@@ -1,14 +1,39 @@
-# From https://github.com/spglib/spglib/blob/d8c39f6/example/python_api/example_full.py#L119-L122
-@testset "Test Niggli reduction from Python example" begin
-    𝐚 = [3, 0, 0]
-    𝐛 = [-3.66666667, 3.68178701, 0]
-    𝐜 = [-0.66666667, -1.3429469, 1.32364995]
-    lattice = Lattice(𝐚, 𝐛, 𝐜)
-    @test niggli_reduce(lattice, 1e-5) ≈ Lattice([
-        [-0.66666667, -1.3429469, 1.32364995],
-        [2.33333333, -1.3429469, 1.32364995],
-        [0.99999999, 0.99589321, 2.6472999],
-    ])
+@testset "Test examples from Python" begin
+    # From https://github.com/spglib/spglib/blob/d8c39f6/example/python_api/example_full.py#L119-L122
+    @testset "Unknown lattice" begin
+        𝐚 = [3, 0, 0]
+        𝐛 = [-3.66666667, 3.68178701, 0]
+        𝐜 = [-0.66666667, -1.3429469, 1.32364995]
+        lattice = Lattice(𝐚, 𝐛, 𝐜)
+        @test niggli_reduce(lattice, 1e-5) ≈ Lattice([
+            [-0.66666667, -1.3429469, 1.32364995],
+            [2.33333333, -1.3429469, 1.32364995],
+            [0.99999999, 0.99589321, 2.6472999],
+        ])  # Compared with Python result
+        @test delaunay_reduce(lattice, 1e-5) ≈ Lattice([
+            [-0.66666667, -1.3429469, 1.32364995],
+            [-2.33333333, 1.3429469, -1.32364995],
+            [-0.99999999, -0.99589321, -2.6472999],
+        ])  # Compared with Python result
+    end
+    # From https://spglib.github.io/spglib/definition.html#computing-rigid-rotation-introduced-by-idealization
+    @testset "Test another lattice" begin
+        lattice = Lattice([
+            [5.0759761474456697, 5.0759761474456697, 0],
+            [-2.8280307701821314, 2.8280307701821314, 0],
+            [0, 0, 8.57154746],
+        ])
+        @test niggli_reduce(lattice, 1e-5) ≈ Lattice([
+            [2.82803077, -2.82803077, 0.0],
+            [-5.07597615, -5.07597615, 0.0],
+            [0.0, 0.0, -8.57154746],
+        ])  # Compared with Python result
+        @test delaunay_reduce(lattice, 1e-5) ≈ Lattice([
+            [2.82803077, -2.82803077, 0.0],
+            [-5.07597615, -5.07597615, 0.0],
+            [0.0, 0.0, -8.57154746],
+        ])  # Compared with Python result
+    end
 end
 
 # From https://github.com/unkcpz/LibSymspg.jl/blob/f342e72/test/runtests.jl#L83-L85
