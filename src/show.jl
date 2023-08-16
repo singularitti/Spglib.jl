39413bc4e18170067b04b3c00f1edb05c603aa0d
singularitti <singularitti@outlook.com>
2023-08-16 05:44:41 -0400

    Update `show` for `SpglibCell`

--- src/show.jl
+++ src/show.jl
@@ -3,23 +3,23 @@ function Base.show(io::IO, ::MIME"text/plain", cell::SpglibCell)
     println(io)
     println(io, " lattice:")
     for row in eachrow(cell.lattice)
-        println(io, "  ", join(row, "  "))
+        println(io, "   ", join(row, "  "))
     end
     num_atom = natoms(cell)
     println(io, " $num_atom atomic positions:")
     for position in cell.positions
-        println(io, "  ", join(position, "  "))
+        println(io, "   ", join(position, "  "))
     end
     println(io, " $num_atom atoms:")
-    println(io, "  ", join(cell.atoms, "  "))
+    println(io, "   ", join(cell.atoms, "  "))
     if !isempty(cell.magmoms)
         println(io, " $num_atom magmoms:")
         if eltype(cell.magmoms) <: AbstractArray
             for magmom in cell.magmoms
-                println(io, "  ", magmom)
+                println(io, "   ", magmom)
             end
         else
-            println(io, "  ", join(cell.magmoms, "  "))
+            println(io, "   ", join(cell.magmoms, "  "))
         end
     end
     return nothing
