2b4965538bb27fe32caa81c445d4ff7216b2cb66
singularitti <singularitti@outlook.com>
2023-10-16 19:35:25 -0400

    Improve the performance of `_expand_cell` greatly by type stability
    from 3.395 μs to 526.084 ns

--- src/core.jl
+++ src/core.jl
@@ -159,7 +159,8 @@ function _expand_cell(cell::SpglibCell)
     # Reference: https://github.com/mdavezac/spglib.jl/blob/master/src/spglib.jl#L32-L35 and https://github.com/spglib/spglib/blob/444e061/python/spglib/spglib.py#L953-L975
     lattice = Base.cconvert(Matrix{Cdouble}, transpose(lattice))   # `transpose` must before `cconvert`!
     positions = Base.cconvert(Matrix{Cdouble}, reduce(hcat, positions))
-    atoms = collect(Cint, findfirst(isequal(u), unique(atoms)) for u in atoms)
+    atomtypes = unique(atoms)
+    atoms = collect(Cint, findfirst(==(atom), atomtypes) for atom in atoms)  # Mapping between unique atom types and atom indices
     if !isempty(magmoms)
         magmoms = if eltype(magmoms) <: AbstractVector
             Base.cconvert(Matrix{Cdouble}, reduce(hcat, magmoms))
@@ -173,7 +174,8 @@ function _expand_cell(cell::CrystallographyCell)
     lattice, positions, atoms = cell.lattice, cell.positions, cell.atoms
     lattice = Base.cconvert(Matrix{Cdouble}, transpose(lattice))   # `transpose` must before `cconvert`!
     positions = Base.cconvert(Matrix{Cdouble}, reduce(hcat, positions))
-    atoms = collect(Cint, findfirst(isequal(u), unique(atoms)) for u in atoms)
+    atomtypes = unique(atoms)
+    atoms = collect(Cint, findfirst(==(atom), atomtypes) for atom in atoms)  # Mapping between unique atom types and atom indices
     return lattice, positions, atoms
 end
 
