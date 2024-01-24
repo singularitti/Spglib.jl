b912ffbb870caacf068e8c317ba16fb7e38db503
singularitti <singularitti@outlook.com>
2024-01-24 17:21:49 -0500

    Wrap constants in enum type `SpglibReturnCode` again

--- src/error.jl
+++ src/error.jl
@@ -1,16 +1,20 @@
 export get_error_code, get_error_message
 
-# Mapping of the `SpglibError` C enum variants to integer values.
-const SPGLIB_SUCCESS = 0
-const SPGERR_SPACEGROUP_SEARCH_FAILED = 1
-const SPGERR_CELL_STANDARDIZATION_FAILED = 2
-const SPGERR_SYMMETRY_OPERATION_SEARCH_FAILED = 3
-const SPGERR_ATOMS_TOO_CLOSE = 4
-const SPGERR_POINTGROUP_NOT_FOUND = 5
-const SPGERR_NIGGLI_FAILED = 6
-const SPGERR_DELAUNAY_FAILED = 7
-const SPGERR_ARRAY_SIZE_SHORTAGE = 8
-const SPGERR_NONE = 9
+# See https://www.mcobject.com/docs/Content/Programming/C/Return_Codes.htm or
+# https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+"Represent various return codes from the Spglib library."
+@enum SpglibReturnCode::Int8 begin
+    SUCCESS = 0
+    SPACEGROUP_SEARCH_FAILED = 1
+    CELL_STANDARDIZATION_FAILED = 2
+    SYMMETRY_OPERATION_SEARCH_FAILED = 3
+    ATOMS_TOO_CLOSE = 4
+    POINTGROUP_NOT_FOUND = 5
+    NIGGLI_FAILED = 6
+    DELAUNAY_FAILED = 7
+    ARRAY_SIZE_SHORTAGE = 8
+    NONE = 9
+end
 
 struct SpglibError <: Exception
     msg::String
@@ -28,15 +32,15 @@ end
 
 Return an instance of the enumerated type `SpglibReturnCode`.
 """
-get_error_code() = @ccall libsymspg.spg_get_error_code()::Cint
+get_error_code() = SpglibReturnCode(@ccall libsymspg.spg_get_error_code()::Cint)
 
 """
     get_error_message(code::SpglibReturnCode)
 
 Get the corresponding error message for a given error code.
 """
-get_error_message(code::Cint) =
-    unsafe_string(@ccall libsymspg.spg_get_error_message(code::Cint)::Cstring)
+get_error_message(code::SpglibReturnCode) =
+    unsafe_string(@ccall libsymspg.spg_get_error_message(Cint(code)::Cint)::Cstring)
 
 """
     check_error()
@@ -45,7 +49,7 @@ Check if an error has occurred and throw an exception if so.
 """
 function check_error()
     code = get_error_code()
-    if code == SPGLIB_SUCCESS
+    if code == SUCCESS
         return nothing
     else
         return throw(SpglibError(get_error_message(code)))
