790157033d8e5414f4ff72a2bcb005abdf829b41
Kipton Barros <kbarros@gmail.com>
2024-01-24 07:48:46 -0700

    Unpack enum manually

--- src/error.jl
+++ src/error.jl
@@ -27,7 +27,42 @@ end
 
 Return an instance of the enumerated type `SpglibReturnCode`.
 """
-get_error_code() = @ccall libsymspg.spg_get_error_code()::SpglibReturnCode
+function get_error_code()
+    # The return value for the C function `spg_get_error_code` is a C enum
+    # called SpglibError. The C compiler guarantees that enums are internally
+    # stored as type `int`; its size is "implementation dependent". Typically 4
+    # or 8 bytes, but this could depend on the architecture and compiler details
+    # https://stackoverflow.com/a/366033/500314. Julia provides `CInt` to
+    # guarantee an integer that has the same size as the C `int` value. Manually
+    # map this enum tag to variants of the `@enum SpglibReturnCode`. The latter
+    # is defined as a Union using the Julia SumTypes library, and its
+    # representation details are also implementation dependent (e.g., the "tag"
+    # of this Union need not have the same size as a `CInt`). Note that a
+    # mistake here could cause memory corruption, as discussed in
+    # https://github.com/singularitti/Spglib.jl/issues/183.
+    code = @ccall libsymspg.spg_get_error_code()::CInt
+    if code == 0
+        return SUCCESS
+    elseif code == 1
+        return SPACEGROUP_SEARCH_FAILED
+    elseif code == 2
+        return CELL_STANDARDIZATION_FAILED
+    elseif code == 3
+        return SYMMETRY_OPERATION_SEARCH_FAILED
+    elseif code == 4
+        return ATOMS_TOO_CLOSE
+    elseif code == 5
+        return POINTGROUP_NOT_FOUND
+    elseif code == 6
+        return NIGGLI_FAILED
+    elseif code == 7
+        return DELAUNAY_FAILED
+    elseif code == 8
+        return ARRAY_SIZE_SHORTAGE
+    elseif code == 9
+        return NONE
+    end
+end
 
 """
     get_error_message(code::SpglibReturnCode)
